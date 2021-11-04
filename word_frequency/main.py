5b8ed559b180b45a0bad75058a609024f2147e9a
lpython202 <84080944+lpython202@users.noreply.github.com>
2021-11-04 17:27:07 +0300

    вывела 10 популярных слов в тексте

--- word_frequency/main.py
+++ word_frequency/main.py
@@ -1,8 +1,20 @@
+from collections import Counter
+
+
 def main():
     path = input('Укажите путь до файла: ')
+    counter = Counter()
+    symbols = ',.!@-—:;()?/'
+    translation_table = dict.fromkeys(map(ord, symbols), None)
+
     with open(path, 'r') as fp:
         for line in fp:
-            print(line)
+            line = line.lower()
+            line = line.translate(translation_table)
+            line_list = line.split()
+            counter.update(line_list)
+    print(counter.most_common(10))
 
 if __name__ == '__main__':
     main()
+
